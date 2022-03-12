79cf7843d181ad83cee8be1844e912eb10666771
nikhil081 <nikmakkad08@gmail.com>
2022-03-13 01:42:38 +0530

    addded function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
 class Counter extends React.Component {
+    state = {
+        count: 1
+    };
     render() {
-        return <h1>hello mdkgdksfk</h1>;
+        return (
+            <div>
+            <span>{this.checkValue()}</span>
+        </div>
+        );
+    }
+    checkValue() {
+        const { count } = this.state;
+        return count === 0 ? "zero" : count;
     }
 }
-
 export default Counter;
