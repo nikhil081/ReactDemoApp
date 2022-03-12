f2cc1cd11e6494266353539d3de4029e6e6e2214
nikhil081 <nikmakkad08@gmail.com>
2022-03-13 01:58:20 +0530

    Added lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
+import React, {  } from 'react';
 
 class Counter extends React.Component {
     state = {
         count: 1,
-        imageUrl:'https://picsum.photos/200'
+        imageUrl:'https://picsum.photos/200',
+        tags:["tag1","tag2","tag3"]
     };
     render() {
         let classes = "badge m-4 badge-";
         classes+=this.state.count===0?"warning":"primary";
         return (
             <div>
-                <img src={this.state.imageUrl}/>
+                <img src={this.state.imageUrl} alt=""/>
             <span className={classes}>{this.checkValue()}</span>
+            <ul>
+                {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
+            </ul>
         </div>
         );
     }
